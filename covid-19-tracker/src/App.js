47f72516341f9b5f85fe4f2594f77133912e8dfd
Mohsin <mohsinsait23@gmail.com>
2020-10-01 23:23:36 +0530

    added feature to select country and added three info cards

--- covid-19-tracker/src/App.js
+++ covid-19-tracker/src/App.js
@@ -1,9 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import { MenuItem, FormControl, Select } from "@material-ui/core";
+import InfoBox from './InfoBox';
 import './App.css';
 
 function App() {
   const [countries, setCountries] = useState([]);
+  const [country, setCountry] = useState("worldwide");
+
   //STATE = How to write a variable in react
 
   // https://disease.sh/v3/covid-19/countries
@@ -14,7 +17,7 @@ function App() {
       .then((data) => {
         const countries = data.map((country) => ({
           name: country.country,
-          value: country.countryInfo.iso
+          value: country.countryInfo.iso2
         }));
 
         setCountries(countries);
@@ -23,13 +26,19 @@ function App() {
 
     getCountriesData();
   }, []);
+
+  const onCountryChange = (event) => {
+    const countryCode = event.target.value;
+    setCountry(countryCode);
+  };
+
   return (
     <div className="app">
       <div className="app__header">
         <h1>COVID-19 TRACKER</h1>
         <FormControl className="ap__dropdown">
-          <Select variant="outlined" value="abc">
-            <MenuItem value={country.value}>{country.name}</MenuItem>
+          <Select variant="outlined" onChange={onCountryChange} value={country}>
+            <MenuItem value="worldwide">Worldwide</MenuItem>
             {
               countries.map(country => (
                 <MenuItem value={country.value}>{country.name}</MenuItem>
@@ -43,6 +52,23 @@ function App() {
           </Select>
         </FormControl>
       </div>
+
+      <div className="app__stats">
+        <InfoBox title="Coronavirus cases" cases={123} total={2000} />
+
+        <InfoBox title="Recovered" cases={1234} total={3000} />
+
+        <InfoBox title="Deaths" cases={12345} total={4000} />
+        {/* InfoBoxs title="coronavirus cases" */}
+        {/* InfoBoxs title="coronavirus cases" */}
+        {/* InfoBoxs title="coronavirus cases" */}
+      </div>
+      
+
+      {/* Table */}
+      {/* Graph */}
+
+      {/* map */}
     </div>
   );
 }
