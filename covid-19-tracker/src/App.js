92386d93d42ed7cedfaebd0857b5ab2457a0b875
unknown <mohsinsait23@gmail.com>
2020-10-03 20:13:16 +0530

    added graph component

--- covid-19-tracker/src/App.js
+++ covid-19-tracker/src/App.js
@@ -5,6 +5,7 @@ import Map from "./Map";
 import './App.css';
 import Table from "./Table";
 import { sortData } from "./util";
+import LineGraph from "./LineGraph";
 
 function App() {
   const [countries, setCountries] = useState([]);
@@ -99,6 +100,7 @@ function App() {
           <h3>Live cases by country</h3>
           <Table countries={tableData} />
           <h3>Worldwide new cases</h3>
+          <LineGraph />
           {/* Graph */}
         </CardContent>
         
