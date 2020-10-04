26e11a830c8163714eae39b0996310e723e6a90c
unknown <mohsinsait23@gmail.com>
2020-10-04 17:47:43 +0530

    updated the circles on the map with interactive tooltip

--- covid-19-tracker/src/App.js
+++ covid-19-tracker/src/App.js
@@ -15,6 +15,7 @@ function App() {
   const [tableData, setTableData] = useState([]);
   const [mapCenter, setMapCenter] = useState({ lat: 34.80746, lng: -40.4796 });
   const [mapZoom, setMapZoom] = useState(3);
+  const [mapCountries, setMapCountries] = useState([]);
 
   useEffect(() => {
     fetch('https://disease.sh/v3/covid-19/all')
@@ -37,6 +38,7 @@ function App() {
 
         const sortedData = sortData(data);
         setTableData(sortedData);
+        setMapCountries(data);
         setCountries(countries);
       });
     };
@@ -115,9 +117,7 @@ function App() {
         
 
         
-        <Map
-          center={mapCenter}
-          zoom={mapZoom} 
+        <Map countries={mapCountries} center={mapCenter} zoom={mapZoom} 
         />
       </div>
       <Card className="app__right">
--- covid-19-tracker/src/util.js
+++ covid-19-tracker/src/util.js
@@ -1,3 +1,23 @@
+import React from "react";
+import numeral from "numeral";
+import { Circle, Popup } from "react-leaflet";
+
+const casesTypeColors = {
+	cases: {
+		hex: "#CC1034",
+		multiplier: 800,
+	},
+	recovered: {
+		hex: "#7dd71d",
+		multiplier: 1200,
+	},
+	deaths: {
+		hex: "#fb4443",
+		multiplier: 2000,
+	}
+}
+
+
 export const sortData = (data) => {
 	const sortedData = [...data];
 
@@ -10,3 +30,23 @@ export const sortData = (data) => {
 	})
 	return sortedData;
 }
+
+// Draw circles on map with interactive tooltip
+export const showDataOnMap = (data, casesType="cases") => (
+	data.map(country => (
+		<Circle
+			center={[country.countryInfo.lat, country.countryInfo.long]}
+			fillOpacity={0.4}
+			color={casesTypeColors[casesType].hex}
+			fillColor={casesTypeColors[casesType].hex}
+			radius={
+				Math.sqrt(country[casesType]) * casesTypeColors[casesType].multiplier
+			}
+		>
+			<Popup>
+				<h1>I am a popup</h1>
+			</Popup>
+
+		</Circle>
+	))
+);
